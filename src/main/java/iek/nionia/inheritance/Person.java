feea3eb4f4485f1e23178d5e2f6863352f30c484
PC2 <PC2@LAB-A-PC10.lan>
2025-01-07 19:54:42 +0200

    class Person

---
+++ src/main/java/iek/nionia/inheritance/Person.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iek.nionia.inheritance;
+
+/**
+ *
+ * @author PC2
+ */
+public class Person {
+
+    private String name;
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String ID;
+
+    /**
+     * Get the value of ID
+     *
+     * @return the value of ID
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * Set the value of ID
+     *
+     * @param ID new value of ID
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    private int age;
+
+    /**
+     * Get the value of age
+     *
+     * @return the value of age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Set the value of age
+     *
+     * @param age new value of age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
+}
+
+    
